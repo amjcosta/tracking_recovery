407a7318a6b49c2d84fd39934a2abe473917f4e8
Amy Costa <costa.amyj@gmail.com>
2017-06-30 09:42:28 -0400

    Updated Food and DailyFoodList models.

--- food_list/models.py
+++ food_list/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
-class DailyFoodList(models.Model):
-    entry_date = models.DateTimeField('date of food')
-
 class Food(models.Model):
-    day_eaten = models.ForeignKey(DailyFoodList, on_delete=models.CASCADE)
     food_text = models.CharField(max_length=200)
     calorie_amount = models.IntegerField(default=0)
+    serving_size = models.CharField(max_length=200)
+    number_of_servings = models.DecimalField(max_digits=100, decimal_places=3)
+    def __str__(self):
+        return self.food_text
+
+class DailyFoodList(models.Model):
+    entry_date = models.DateTimeField('date of food')
+    food_text = models.ForeignKey(Food, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.entry_date.strftime("%B %d, %Y")
