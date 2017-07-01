bb6d5a4eb337b596a60a632f928957752ddbf340
Amy Costa <costa.amyj@gmail.com>
2017-06-30 21:54:29 -0400

    Adding views.

--- food_list/urls.py
+++ food_list/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # ex: /food_list
     url(r'^$', views.index, name='index'),
+    # ex: /food_list/20170630
+    url(r'^(?P<dailyfoodlist_id>[0-9]+)/$', views.daily_list, name='daily_list'),
 ]
--- food_list/views.py
+++ food_list/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import DailyFoodList
 
 def index(request):
-    return HttpResponse("Hello, world! You can list your foods here.")
+    recent_days_list = DailyFoodList.objects.order_by('-entry_date')[:7]
+    template = loader.get_template("food_list/index.html")
+    context = {
+        'recent_days_list': recent_days_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+def daily_list(request, dailyfoodlist_id):
+    response = "Meal plan for %s:"
+    return HttpResponse(response % dailyfoodlist_id)
